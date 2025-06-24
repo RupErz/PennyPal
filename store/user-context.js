a3248d197c5e3eae35f5d4a080c94229d63bd7a3
Nghia <nghiavu144@gmail.com>
2025-06-23 19:05:22 -0500

    Update User Context with dailyReminders

--- store/user-context.js
+++ store/user-context.js
@@ -6,16 +6,19 @@ export const UserContext = createContext({
     monthlyIncome: 0,
     hasCompletedOnboarding: false,
     onboardingStep: "Welcome",
+    dailyReminders: false,
     setUserName: () => {},
     setMonthlyIncome: () => {},
     setHasCompletedOnboarding: () => {},
     setOnboardingStep: () => {},
+    setDailyReminders: () => {},
 })
 
 export const UserContextProvider = ({children}) => {
     const [userName, setUserName] = useState("")
     const [monthlyIncome, setMonthlyIncome] = useState(0)
     const [hasCompletedOnboarding, setHasCompletedOnboarding] = useState(false)
+    const [dailyReminders, setDailyReminders] = useState(false)
 
     // Tracking onboarding step
     const [onboardingStep, setOnboardingStep] = useState("Welcome")
@@ -29,17 +32,20 @@ export const UserContextProvider = ({children}) => {
                 const storedIncome = await AsyncStorage.getItem("monthlyIncome")
                 const onboardingComplete = await AsyncStorage.getItem("hasCompletedOnboarding")
                 const storedOnboardingStep = await AsyncStorage.getItem("onboardingStep")
+                const storedDailyReminders = await AsyncStorage.getItem("dailyReminders")
 
                 // Debugging logs
                 console.log('Loaded userName:', storedName);
                 console.log('Loaded monthlyIncome:', storedIncome);
                 console.log('Loaded hasCompletedOnboarding:', onboardingComplete)
                 console.log('Loaded onboardingStep:', storedOnboardingStep);
+                console.log('Loaded dailyReminders:', storedDailyReminders);
 
                 if (storedName) setUserName(storedName)
                 if (storedIncome) setMonthlyIncome(Number(storedIncome))
                 if (onboardingComplete) setHasCompletedOnboarding(onboardingComplete === "true")
-                if (storedOnboardingStep) setOnboardingStep(storedOnboardingStep)      
+                if (storedOnboardingStep) setOnboardingStep(storedOnboardingStep)   
+                if (storedDailyReminders) setDailyReminders(storedDailyReminders === "true")   
             } catch (error) {
                 console.log("Failed to load user data:",  error)
             } finally {
@@ -54,18 +60,19 @@ export const UserContextProvider = ({children}) => {
     useEffect(() => {
         const saveUserData = async () => {
             try {
-            await AsyncStorage.setItem("userName", userName);
-            await AsyncStorage.setItem("monthlyIncome", monthlyIncome.toString());
-            await AsyncStorage.setItem("hasCompletedOnboarding", hasCompletedOnboarding.toString());
-            await AsyncStorage.setItem("onboardingStep", onboardingStep);
-            // console.log("User data saved successfully!"); Debugging Log
+                await AsyncStorage.setItem("userName", userName);
+                await AsyncStorage.setItem("monthlyIncome", monthlyIncome.toString());
+                await AsyncStorage.setItem("hasCompletedOnboarding", hasCompletedOnboarding.toString());
+                await AsyncStorage.setItem("onboardingStep", onboardingStep);
+                await AsyncStorage.setItem("dailyReminders", dailyReminders.toString())
+                // console.log("User data saved successfully!"); Debugging Log
             } catch (error) {
                 console.log("Failed to save user data:", error);
             }
         };
 
         saveUserData();
-    }, [userName, monthlyIncome, hasCompletedOnboarding, onboardingStep]);
+    }, [userName, monthlyIncome, hasCompletedOnboarding, onboardingStep, dailyReminders]);
 
 
     // Provide the context values
@@ -76,10 +83,12 @@ export const UserContextProvider = ({children}) => {
                 monthlyIncome,
                 hasCompletedOnboarding,
                 onboardingStep,
+                dailyReminders,
                 setUserName,
                 setMonthlyIncome,
                 setHasCompletedOnboarding,
                 setOnboardingStep,
+                setDailyReminders,
                 isLoading // expose loading state
             }}
         >
